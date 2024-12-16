8609afc187aa1b35aef8c33eb84607d951beab53
tighnaridotme <35395586+tighnaridotme@users.noreply.github.com>
2024-12-16 22:36:15 +0100

    Add services container

--- assets/main.js
+++ assets/main.js
@@ -12,11 +12,13 @@ async function fetchRecentTrack() {
         const infoContainer = document.createElement('div');
         infoContainer.id = "info";
 
-        const albumCoverElem = document.createElement('img');
-        albumCoverElem.id = "artwork";
-        albumCoverElem.src = data.albumCover;
-        nowplaying.appendChild(albumCoverElem);
-
+        const artworkContainer = document.createElement('div');
+        artworkContainer.id = "artworkContainer"
+        const artworkElem = document.createElement('img');
+        artworkElem.id = "artwork";
+        artworkElem.src = data.albumCover;
+        artworkContainer.appendChild(artworkElem);
+        nowplaying.appendChild(artworkContainer);
 
         const trackLinkElem = document.createElement('a');
         trackLinkElem.id = "track";
@@ -56,7 +58,37 @@ async function fetchRecentTrack() {
         }
         infoContainer.appendChild(statusElem);
 
+        const services = document.createElement('div');
+        services.id = "services";
+
+        const youtubeLink = document.createElement('a');
+        youtubeLink.id = "youtube";
+        youtubeLink.title = "Youtube search";
+        youtubeLink.href = `https://www.youtube.com/results?search_query=${data.artist.replaceAll(' ', '+')}+${data.track.replaceAll(' ', '+')}`;
+        youtubeLink.target = "_blank";
+        youtubeLink.classList.add('icon');
+        
+        const youtubeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        youtubeIcon.classList.add('iconSvg');
+        youtubeIcon.innerHTML = `<use xlink:href="#youtubeIcon"></use>`;
+        youtubeLink.appendChild(youtubeIcon);
+        services.appendChild(youtubeLink);
+
+        const spotifyLink = document.createElement('a');
+        spotifyLink.id = "spotify";
+        spotifyLink.title = "Open in Spotify";
+        spotifyLink.href = data.spotifyLink;
+        spotifyLink.target = "_blank";
+        spotifyLink.classList.add('icon');
+        
+        const spotifyIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        spotifyIcon.classList.add('iconSvg');
+        spotifyIcon.innerHTML = `<use xlink:href="#spotifyIcon"></use>`;
+        spotifyLink.appendChild(spotifyIcon);
+        services.appendChild(spotifyLink);
+
         nowplaying.appendChild(infoContainer);
+        nowplaying.appendChild(services);
     } catch (error) {
         console.error("Error fetching recent track:", error);
     }
@@ -73,6 +105,8 @@ async function refetch() {
             const artistElem = document.getElementById('artist');
             const statusElem = document.getElementById('status');
             const artworkElem = document.getElementById('artwork');
+            const youtubeLink = document.getElementById('youtube');
+            const spotifyLink = document.getElementById('spotify');
 
             trackElem.href = data.trackLink;
             trackElem.textContent = data.track;
@@ -97,6 +131,8 @@ async function refetch() {
             }
 
             artwork.src = data.albumCover;
+            youtubeLink.href = `https://www.youtube.com/results?search_query=${data.artist.replaceAll(' ', '+')}+${data.track.replaceAll(' ', '+')}`;
+            spotifyLink.href = data.spotifyLink;
         }
     } catch (error) {
         console.error("Error fetching recent track:", error);
